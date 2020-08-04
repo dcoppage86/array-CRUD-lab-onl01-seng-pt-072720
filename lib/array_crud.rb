7e7621946c1242be6b70a6c3b6578a983833ee1c
Dakota Coppage <dakotakcoppage@me.com>
2020-08-04 17:08:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array
   array = ["Fyn", "Kov", "Arlo", "Un-named"]
-  array.push("Savannah", "Dakota")
+  array << "Savannah" << "Dakota"
 end
 
 def add_element_to_start_of_array(array, element)
